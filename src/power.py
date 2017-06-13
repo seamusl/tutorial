7a324166d70e8acccf40f6c11eae55e79848af83
seamusl <seamus.lankford@cit.ie>
2017-06-13 15:16:51 +0100

    Testing Merge Conflicts on Website

--- src/power.py
+++ src/power.py
@@ -1,3 +1,7 @@
+""" This is a script that calculates one number to
+    the power of another.
+"""
+
 def to_the_power(x, y = 2):
     result = x
     for i in range(0, y):
@@ -5,5 +9,5 @@ def to_the_power(x, y = 2):
     return result
 
 x = 9
-y = 6
+y = 8
 print("{0} to the power of {1} is: {2}".format(x, y, to_the_power(x, y)))
